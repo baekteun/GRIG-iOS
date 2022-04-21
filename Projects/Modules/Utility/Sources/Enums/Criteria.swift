e6b96d5d4ad175e2df95335271eac4b114d26bc9
baegteun <baegteun@gmail.com>
2022-04-21 20:20:17 +0900

    :sparkles: :: Criteria Enums

---
+++ Projects/Modules/Utility/Sources/Enums/Criteria.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public enum Criteria: String {
+    case contributions = "contributions"
+    case stared = "stared"
+    case following = "following"
+    case follower = "follower"
+    case forked = "forked"
+    case issues = "issues"
+    case pullRequests = "pullRequests"
+}
