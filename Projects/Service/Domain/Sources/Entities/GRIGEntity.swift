8f5f40a2620d5bf51f669e18edc73e313249c257
baegteun <baegteun@gmail.com>
2022-04-22 08:21:50 +0900

    :sparkles: :: GRIGEntity

---
+++ Projects/Service/Domain/Sources/Entities/GRIGEntity.swift
@@ -0,0 +1,9 @@
+public struct GRIGEntity: Equatable {
+    public let name: String
+    public let nickname: String
+    public let generation: Int
+    public let bio: String
+    public let avatarUrl: String
+    public let result: Int
+}
+
