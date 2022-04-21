65a8f38ce4ef41a5b0806e33058bbed7b813ec42
baegteun <baegteun@gmail.com>
2022-04-21 17:01:51 +0900

    :sparkles: :: ViewControllable extension

--- Projects/Modules/ThirdPartyLib/Sources/ThirdPartyLibrary.swift
+++ Projects/Modules/ThirdPartyLib/Sources/ThirdPartyLibrary.swift
@@ -1,1 +1,80 @@
-// this is for tuist
+import RIBs
+import UIKit
+
+public extension ViewControllable {
+    func present(_ viewControllable: ViewControllable, animated: Bool, completion: (() -> Void)?) {
+        self.uiviewController.present(viewControllable.uiviewController, animated: animated, completion: completion)
+    }
+    func dismiss(animated : Bool,completion: (() -> Void)?) {
+        self.uiviewController.dismiss(animated: animated, completion: completion)
+    }
+    func presentAlert(
+        title: String?,
+        message: String?,
+        style: UIAlertController.Style,
+        actions: [UIAlertAction] = []
+    ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach(alert.addAction(_:))
+        self.uiviewController.navigationController?.visibleViewController?.present(alert, animated: true)
+    }
+    func presentFailureAlert(
+        title: String?,
+        message: String?,
+        style: UIAlertController.Style,
+        actions: [UIAlertAction]? = nil
+    ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        if let actions = actions {
+            actions.forEach(alert.addAction(_:))
+        } else {
+            alert.addAction(.init(title: "확인", style: .cancel, handler: nil))
+        }
+        self.uiviewController.navigationController?.visibleViewController?.present(alert, animated: true)
+    }
+    func pushViewController(_ viewControllable: ViewControllable, animated: Bool) {
+        if let nav = self.uiviewController as? UINavigationController {
+            nav.pushViewController(viewControllable.uiviewController, animated: animated)
+        } else {
+            self.uiviewController.navigationController?.pushViewController(
+                viewControllable.uiviewController,
+                animated: animated
+            )
+        }
+    }
+    func popViewController(animated: Bool) {
+        if let nav = self.uiviewController as? UINavigationController {
+            nav.popViewController(animated: animated)
+        } else {
+            self.uiviewController.navigationController?.popViewController(animated: animated)
+        }
+    }
+    func popToRoot(animated: Bool) {
+        if let nav = self.uiviewController as? UINavigationController {
+            nav.popToRootViewController(animated: animated)
+        } else {
+            self.uiviewController.navigationController?.popToRootViewController(animated: animated)
+        }
+    }
+    func setViewControllers(_ viewControllerables: [ViewControllable]) {
+        if let nav = self.uiviewController as? UINavigationController {
+            nav.setViewControllers(viewControllerables.map(\.uiviewController), animated: true)
+        } else {
+            self.uiviewController.navigationController?.setViewControllers(
+                viewControllerables.map(\.uiviewController),
+                animated: true
+            )
+        }
+    }
+    func setViewControllers(_ viewControllers: [UIViewController], animated: Bool) {
+        guard let tab = self.uiviewController as? UITabBarController else { return }
+        tab.setViewControllers(viewControllers, animated: animated)
+    }
+    var topViewControllable: ViewControllable {
+        var top: ViewControllable = self
+        while let presented = top.uiviewController.presentedViewController as? ViewControllable {
+            top = presented
+        }
+        return top
+    }
+}
