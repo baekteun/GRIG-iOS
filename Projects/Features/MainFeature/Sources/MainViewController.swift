7051a7d9afd59c7fa29bc3bdb12b592e8d43abea
baegteun <baegteun@gmail.com>
2022-04-29 09:30:03 +0900

    :lipstick: :: Main UI

--- Projects/Features/MainFeature/Sources/MainViewController.swift
+++ Projects/Features/MainFeature/Sources/MainViewController.swift
@@ -29,6 +29,18 @@ final class MainViewController: BaseViewController, MainPresentable, MainViewCon
         $0.isHidden = true
         $0.register(cellType: RankCollectionCell.self)
     }
+    private let logoImageView = UIImageView(image: CoreAsset.Images.grigLogo.image.withRenderingMode(.alwaysOriginal))
+    private let helpButton = UIButton().then {
+        $0.setImage(.init(systemName: "questionmark.circle.fill"), for: .normal)
+        $0.tintColor = .init(red: 0.6, green: 0.6, blue: 0.6, alpha: 0.6)
+    }
+    private let sortButton = UIButton().then {
+        $0.setTitle("contributions | All", for: .normal)
+        $0.setTitleColor(.init(red: 0.6, green: 0.6, blue: 0.6, alpha: 0.6), for: .normal)
+        $0.setImage(.init(systemName: "chevron.down"), for: .normal)
+        $0.tintColor = .init(red: 0.6, green: 0.6, blue: 0.6, alpha: 0.6)
+        $0.semanticContentAttribute = .forceRightToLeft
+    }
 
     weak var listener: MainPresentableListener?
     
@@ -50,11 +62,26 @@ final class MainViewController: BaseViewController, MainPresentable, MainViewCon
     
     // MARK: - UI
     override func addView() {
-        view.addSubviews(rankTableView, rankCollectionView)
+        view.addSubviews(rankTableView, logoImageView, helpButton, sortButton)
     }
     override func setLayout() {
+        logoImageView.snp.makeConstraints {
+            $0.top.centerX.equalTo(view.safeAreaLayoutGuide)
+            $0.width.equalTo(bounds.width*0.288)
+            $0.height.equalTo(bounds.width*0.288*0.3)
+        }
+        helpButton.snp.makeConstraints {
+            $0.centerY.equalTo(logoImageView)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.size.equalTo(30)
+        }
         rankTableView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.equalTo(logoImageView.snp.bottom).offset(bounds.height*0.1)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        sortButton.snp.makeConstraints {
+            $0.trailing.equalTo(rankTableView.snp.trailing).inset(20)
+            $0.bottom.equalTo(rankTableView.snp.top).offset(-8)
         }
     }
     override func configureVC() {
--- Projects/Modules/Core/Derived/Sources/Assets+Core.swift
+++ Projects/Modules/Core/Derived/Sources/Assets+Core.swift
@@ -25,6 +25,7 @@ public enum CoreAsset {
     public static let grigWhite = CoreColors(name: "GRIG_White")
   }
   public enum Images {
+    public static let grigLogo = CoreImages(name: "GRIG_Logo")
   }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
@@ -61,6 +62,46 @@ public extension CoreColors.Color {
     self.init(named: asset.name, in: bundle, compatibleWith: nil)
     #elseif os(macOS)
     self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+public struct CoreImages {
+  public fileprivate(set) var name: String
+
+  #if os(macOS)
+  public typealias Image = NSImage
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  public typealias Image = UIImage
+  #endif
+
+  public var image: Image {
+    let bundle = CoreResources.bundle
+    #if os(iOS) || os(tvOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    let image = bundle.image(forResource: NSImage.Name(name))
+    #elseif os(watchOS)
+    let image = Image(named: name)
+    #endif
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+}
+
+public extension CoreImages.Image {
+  @available(macOS, deprecated,
+    message: "This initializer is unsafe on macOS, please use the CoreImages.image property")
+  convenience init?(asset: CoreImages) {
+    #if os(iOS) || os(tvOS)
+    let bundle = CoreResources.bundle
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSImage.Name(asset.name))
     #elseif os(watchOS)
     self.init(named: asset.name)
     #endif
