5a19e8996604590684ea932fc3d798c4c985861b
baegteun <baegteun@gmail.com>
2022-05-01 12:07:58 +0900

    :sparkles: :: PaddingLabel

---
+++ Projects/Features/UserFeature/Sources/View/PaddingLabel.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+final class PaddingLabel: UILabel {
+    private var padding = UIEdgeInsets(top: 16.0, left: 16.0, bottom: 16.0, right: 16.0)
+
+    convenience init(padding: UIEdgeInsets) {
+        self.init()
+        self.padding = padding
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+
+        return contentSize
+    }
+}
